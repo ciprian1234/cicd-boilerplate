990537eb29f0f585ba91252d65e50b069dfd3c6f
Ciprian Andrei <andrei.stefanel.ciprian@gmail.com>
2020-11-10 23:26:37 +0200

    changed message from backend root path

--- backend/src/main.js
+++ backend/src/main.js
@@ -15,7 +15,7 @@ async function main() {
   app.use(morgan('dev'));
 
   // routes
-  app.get('', (req, res) => res.send('Welcome to next ventures group backend!'));
+  app.get('', (req, res) => res.send('Welcome to the backend!'));
   app.use('/api', apiRouter);
   app.use((req, res, next) => res.status(404).json({ error: 'Not Found!' }));
 
