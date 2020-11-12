805ad6d4f0f4b3ef669fa709af53fc6a9614db77
Ciprian Andrei <andrei.stefanel.ciprian@gmail.com>
2020-11-12 18:04:24 +0200

    removed folder with static asseets before deployment

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -3,6 +3,9 @@
 # create image for building react app
 docker build -f frontend/build.dockerfile -t build-react-app ./frontend
 
+# delete old website static assets
+rm -rf ~/projects/nginx/websites/cicd-boilerplate/* 
+
 # create cointainer copy build content and remove container
 docker create --name dummy build-react-app
 docker cp dummy:/frontend/build ~/projects/nginx/websites/cicd-boilerplate
