f044d10b88941945b21347504d572d6b8d0034a9
Ciprian Andrei <andrei.stefanel.ciprian@gmail.com>
2020-11-12 22:05:22 +0200

    frontend prod

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+let API_URL = 'http://localhost:4000/api';
+if (process.env.NODE_ENV === 'production') {
+  API_URL = 'http://cippau.go.ro/api/api';
+}
+
 function App() {
   const [data, setData] = useState({ empty: 'true' });
   React.useEffect(() => {
     async function fetchData() {
+      console.log('API_URL', process.env);
       // execute an api call to the backend
-      const response = await axios({ url: 'http://localhost:4000/api' });
+      const response = await axios({ url: API_URL });
       setData(response.data);
     }
     fetchData();
