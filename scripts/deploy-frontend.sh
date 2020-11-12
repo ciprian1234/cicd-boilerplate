f6b8d2c171fc1c8c013b796c5068d34f222143f3
Ciprian Andrei <andrei.stefanel.ciprian@gmail.com>
2020-11-12 16:06:58 +0200

    Update deploy-frontend.sh

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -6,7 +6,7 @@ docker build -f build.dockerfile -t build-react-app .
 
 # create cointainer copy build content and remove container
 docker create --name dummy build-react-app
-docker cp dummy:/frontend/build ~/projects/nginx/cicd-boilerplate
+docker cp dummy:/frontend/build ~/projects/nginx/websites/cicd-boilerplate
 
 # cleanup
 docker rm -f dummy
